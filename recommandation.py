e40df8d29ffb8e1e6130ec3f3cce45d4d5892d04
supremepoulet <supreme7@live.fr>
2024-07-05 10:39:08 +0200

    Update recommandation.py

--- recommandation.py
+++ recommandation.py
@@ -24,28 +24,7 @@ from sklearn.metrics import confusion_matrix
 ##############
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from wordcloud import WordCloud
-from nltk.corpus import stopwords
-
-import nltk
-from nltk.tokenize import word_tokenize
-from unidecode import unidecode
-import unicodedata
-import re
-import math
-
-# Initialiser la variable des mots vides
-#stop_words = list(stopwords.words('french'))
-
 import spacy
-from spacy.lang.fr.stop_words import STOP_WORDS
-
-stop_words_fr = list(spacy.lang.fr.stop_words.STOP_WORDS)
-
-wc = WordCloud(background_color="white", max_words=100, stopwords=stop_words_fr, max_font_size=50, random_state=42)
-
-#stop_words.extend('nbsp'.split())
-stop_words_fr.extend('nbsp'.split())
-
 import time
 
 import selenium.webdriver
@@ -54,6 +33,20 @@ import requests
 
 
 import pickle
+# Initialiser la variable des mots vides
+#stop_words = list(stopwords.words('french'))
+
+
+from spacy.lang.fr.stop_words import STOP_WORDS
+
+stop_words_fr = list(spacy.lang.fr.stop_words.STOP_WORDS)
+stop_words_fr.extend('nbsp'.split())
+
+wc = WordCloud(background_color="white", max_words=100, stopwords=stop_words_fr, max_font_size=50, random_state=42)
+
+
+
+
 
 ###############
 ###############
